5cdcc42d9ebdee9ba46d1fe02afc2318965ff77f
Karthik-CT <karthik.iyer@clevertap.com>
2022-02-25 15:46:41 +0530

    First Commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+clevertap.onUserLogin.push({
+  Site: {
+    Name: "Snow John",
+    Identity: 68877102,
+    Email: "snow@gmail.com",
+    Phone: "+91155551234",
+    Gender: "M",
+    DOB: new Date(),
+
+    "MSG-email": true,
+    "MSG-push": true,
+    "MSG-sms": true,
+    "MSG-whatsapp": true,
+  },
+});
