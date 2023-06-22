22bebf4388ffecf5f7f73087ccd35034956c36a7
Karthik Iyer <karthik.iyer@clevertap.com>
2023-06-22 19:39:41 +0530

    added_web_native_display

--- scripts.js
+++ scripts.js
@@ -1,15 +1,39 @@
-clevertap.onUserLogin.push({
-  Site: {
-    Name: "Snow John",
-    Identity: 68877102,
-    Email: "snow@gmail.com",
-    Phone: "+91155551234",
-    Gender: "M",
-    DOB: new Date(),
+function loginUser() {
+  clevertap.onUserLogin.push({
+    Site: {
+      Name: document.getElementById("name").value,
+      Identity: document.getElementById("identity").value,
+      Email: document.getElementById("emailid").value,
+      Phone: document.getElementById("mobno").value,
+      "MSG-email": true,
+      "MSG-push": true,
+      "MSG-sms": true,
+      "MSG-whatsapp": true,
+    },
+  });
+  clevertap.event.push("Web Login Successful");
+  //snackbar
+  var x = document.getElementById("sb6");
+  x.classList.add("show");
+  setTimeout(function () {
+    x.className = x.className.replace("show1", "");
+  }, 3000);
 
-    "MSG-email": true,
-    "MSG-push": true,
-    "MSG-sms": true,
-    "MSG-whatsapp": true,
-  },
-});
+  console.log(clevertap.getCleverTapID());
+}
+
+// clevertap.onUserLogin.push({
+//   Site: {
+//     Name: "Snow John",
+//     Identity: 68877102,
+//     Email: "snow@gmail.com",
+//     Phone: "+91155551234",
+//     Gender: "M",
+//     DOB: new Date(),
+
+//     "MSG-email": true,
+//     "MSG-push": true,
+//     "MSG-sms": true,
+//     "MSG-whatsapp": true,
+//   },
+// });
