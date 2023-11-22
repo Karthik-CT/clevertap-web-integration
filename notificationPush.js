3a289e60a8e9a06bda27768322e1a33f95d0342b
Karthik Iyer <100284000+Karthik-CT@users.noreply.github.com>
2023-11-22 17:16:49 +0530

    Update notificationPush.js

--- notificationPush.js
+++ notificationPush.js
@@ -5,7 +5,7 @@ clevertap.notifications.push({
   okButtonText: "OK",
   rejectButtonText: "Cancel",
   okButtonColor: "#f28046",
-  serviceWorkerPath: "clevertap-web-integration/clevertap_sw.js",
+  serviceWorkerPath: "/service-worker.js",
   okCallback: function () {
     console.log("For Soft OK Popup");
     Notification.requestPermission().then(function (getperm) {
